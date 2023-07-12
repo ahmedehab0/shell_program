a2b8ac6b1279668689247ac4fa180ee5fabba6c3
ahmedehab0 <ahmed.eh01@gmail.com>
2023-07-12 16:48:27 -0400

    tested

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 void _isatty()
 {
 	if (isatty(STDIN_FILENO))
-		_puts("($) ");
+		printf("($) ");
 }
 /**
  *sig_handler - handels the sigint (CTR - c) signal
@@ -13,7 +13,7 @@ void _isatty()
  */
 void sig_handler(int sig)
 {
-	_puts("\n");
+	puts("\n");
 }
 /**
  * main - simple shell program  
@@ -32,7 +32,7 @@ int main(int ac, char **av)
 	while (1)
 	{
 		_isatty();
-		nread = _getline(&lineptr, &n, stdin);
+		nread = getline(&lineptr, &n, stdin);
 		if (nread == -1)
 		{
 			perror("Error: ");
@@ -41,10 +41,11 @@ int main(int ac, char **av)
 		if (nread == EOF)
 			exit(1);
 
-			arg = string_parse(lineptr);
-			execute(arg);
+			/**arg = string_parse(lineptr);
+			*execute(arg);
+			*/
 	}
 	free(lineptr);
-	free(arg);
+	//free(arg);
 	return (0);
 }
