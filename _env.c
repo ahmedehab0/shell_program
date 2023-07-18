22a713b733ce3b5d83855e18f3a80fcb9f993fd6
HanMohamed <hmohamed2@seattleu.edu>
2023-07-17 17:54:38 -0700

    setenv() & unsetenv()

--- _env.c
+++ _env.c
@@ -9,25 +9,99 @@
  */
 char *_getenv(const char *var_name)
 {
-	int i, j;
-	int is_equal;
+	list_env *p;
 
-	for (i = 0; environ[i] != NULL; i++)
+	if(env_head == NULL)
+		env_head = list_environment();
+
+	p = env_head;
+	while (p)
 	{
-		is_equal = 1;
-
-		for (j = 0; environ[i][j] != '='; j++)
-		{
-			if (var_name[j] != environ[i][j])
-			{
-				is_equal = 0;
-				break;
-			}
-		}
-		if (is_equal)
-			return (&environ[i][j + 1]);
+		/* p->name == var_name) */
+		if (*(p->name) == *var_name)
+			return (p->value);
+		p = p->next_env;
 	}
 	return (NULL);
+}
+
+/**
+ * _setenv - changes or adds an environment variable
+ * @name: name of environment
+ * @value: value of environment
+ * @overwrite: new value of environment
+ *
+ * If name does not exist in the environment:
+ * adds the variable name to the environment with the value.
+ *
+ * If name does exist in the environment:
+ * then its value is changed to value if overwrite is nonzero;
+ * if overwrite is zero, then the value of name is not changed
+ * (and setenv() returns a success status)
+ *
+ * Return: return zero on success, or -1 on error,
+ * with errno set to indicate the error.
+ */
+int _setenv(const char *name, const char *value, int overwrite)
+{
+	list_env *p;
+
+	if(env_head == NULL)
+		env_head = list_environment();
+
+	p = env_head;
+	while (p)
+	{
+		if (p->name == name)
+		{
+			if (*value != 0)
+				p->value = (char *) overwrite;
+			return (0);
+		}
+		p = p->next_env;
+	}
+	p = malloc(sizeof(list_env));
+	if (p == NULL)
+		return (-1);
+
+	p->name = strdup(name);
+	p->value = strdup(value);
+	p->next_env = NULL;
+	return (0);
+}
+
+/**
+ * _unsetenv - deletes the variable name from the environment
+ * @name: name of environment
+ *
+ * If name does not exist in the environment, then the function succeeds,
+ * and the environment is unchanged.
+ *
+ * Return: return zero on success, or -1 on error,
+ * with errno set to indicate the error.
+ */
+int _unsetenv(const char *name)
+{
+	list_env *prev, *current;
+
+	if(env_head == NULL)
+		env_head = list_environment();
+
+	prev = NULL;
+	current = env_head;
+
+	while (current)
+	{
+		if (current->name == name)
+		{
+			prev->next_env = current->next_env;
+			return (0);
+		}
+		prev = current;
+		current = current->next_env;
+	}
+	/* when to return (-1) ? */
+	return (0);
 }
 
 /**
@@ -38,37 +112,23 @@ void _printenv(void)
 {
 	char *buff;
 	int letters, wr_cmd;
-	int i = 0;
+	list_env *p;
 
-	while (environ[i])
+	if(env_head == NULL)
+		env_head = list_environment();
+
+	p = env_head;
+	while (p)
 	{
-		letters = _strlen(environ[i]);
-		buff = _strdup(environ[i]);
-		buff[letters] = '\n';
-		wr_cmd = write(STDOUT_FILENO, buff, (letters + 1));
-		free(buff);
+		buff = _strdup(p->name);
+		buff = str_concat(buff, "=");
+		buff = str_concat(buff, p->value);
+		letters = _strlen(buff);
+		wr_cmd = write(STDOUT_FILENO, buff, letters);
 
 		if (wr_cmd == -1)
 			perror("Error");
-		i++;
+		free(buff);
+		p = p->next_env;
 	}
-}
-
-/**
- * _strlen - get length of string
- * @s: A pointer to char
- *
- * computes the length of the string s up to,
- * but not including the terminating null character.
- *
- * Return: length of string
- */
-int _strlen(const char *s)
-{
-	int len;
-
-	for (len = 0; s[len]; len++)
-		;
-
-	return (len);
 }
