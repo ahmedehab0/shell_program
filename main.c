46c96d2abac6c5ec4a260201867501c33a8a7a10
ahmedehab0 <ahmed.eh01@gmail.com>
2023-07-19 18:37:26 -0400

    fixed

--- main.c
+++ main.c
@@ -22,17 +22,12 @@ void sig_handler(int sig)
  *@av: string argument
  *Return: 0
  */
-<<<<<<< HEAD
-int main(int ac __attribute__((unused)), char **av, char *envp[] __attribute__((unused)))
-=======
-int main(int ac __attribute__((unused)), __attribute__((unused)) char **av, __attribute__((unused)) char *envp[])
->>>>>>> 9976b3fc9910a48a378035ebe6d98dc6d05c882d
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)), char *envp[] __attribute__((unused)))
 {
 	ssize_t nread;
 	size_t n;
 	char **arg, *lineptr = NULL,
 	*delim = " ";
-	(void) shell_name;
 
 	signal(SIGINT, sig_handler);
 	while (1)
