8d00633bf400dc19891dbc3d61aa7a086080c2d5
HanMohamed <hmohamed2@seattleu.edu>
2023-07-19 19:10:57 -0700

    list_environment() without arguments

--- main.h
+++ main.h
@@ -75,7 +75,7 @@ typedef struct list_env
 }list_env;
 
 list_env *env_head;
-list_env *add_env_end(list_env *head, char *str);
+list_env *add_env_end(char *str);
 list_env *list_environment(void);
 void _printenv(void);
 void free_list_env(void);
